e0dcf898f0517c67f087fbb1309829337abdfc64
jingwang.tian <jingwang.tian@lengjing.info>
2016-09-27 15:48:29 +0800

    异常目录(浙江)

--- ZheJiang/Business_Abnormal_ZheJiang.py
+++ ZheJiang/Business_Abnormal_ZheJiang.py
@@ -41,6 +41,7 @@ class ZheJiangSearcher(Searcher):
 			params = {"pagination.currentPage": num,'pagination.pageSize': '10'}
 			r = self.post_request(url=url, params=params)
 			r_text = r.text
+			print r_text.rindex(']')
 			if r_text.rindex(']') == -1:
 				# continue
 				r_text = r_text + '}]'
