d1b46e8e1a7875167b1460e097f22e8636ec0e69
jingwang.tian <jingwang.tian@lengjing.info>
2016-09-28 11:23:14 +0800

    异常目录

---
+++ DataBase/MSSQL.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+import pyodbc
+import traceback
+
+
+database_client_connection = None
+database_client_cursor = None
+
+
+def build_connection():
+    global database_client_connection, database_client_cursor
+    try:
+        if database_client_connection:
+            database_client_connection.close()
+    except Exception, e:
+        traceback.print_exc(e)
+    try:
+        database_client_connection = pyodbc.connect('DRIVER={SQL SERVER};SERVER=172.16.0.26;DATABASE=pachong;UID=likai;PWD=d!{<kN5K38u-')
+        database_client_cursor = database_client_connection.cursor()
+    except Exception, e:
+        traceback.print_exc(e)
+        build_connection()
+
+build_connection()
+
+
+def execute_update(sql):
+    global database_client_connection, database_client_cursor
+    try:
+        database_client_cursor.execute(sql)
+        database_client_connection.commit()
+    except Exception, e:
+        traceback.print_exc(e)
+        build_connection()
+        execute_update(sql)
+
+
+def execute_query(sql):
+    global database_client_connection, database_client_cursor
+    try:
+        database_client_cursor.execute(sql)
+        return database_client_cursor.fetchall()
+    except Exception, e:
+        traceback.print_exc(e)
+        build_connection()
+        return execute_query(sql)
+
---
+++ DataBase/MYSQL.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+
+import mysql.connector
+import traceback
+
+
+database_client_connection = None
+database_client_cursor = None
+
+
+def build_connection():
+	config = {'host':'172.16.0.20',#服务器地址
+			  'user':'qianjing',#数据库账号
+			  'password':'zxcASDqwe!@#',#数据库密码
+			  'port':'3306',#默认3306
+			  'database':'enterprise_name',#数据库名
+			  'charset':'utf8',#默认utf8
+
+	}
+	global database_client_connection, database_client_cursor
+	try:
+		if database_client_connection:
+			database_client_connection.close()
+	except Exception, e:
+		traceback.print_exc(e)
+	try:
+		database_client_connection = mysql.connector.connect(**config)
+		database_client_cursor = database_client_connection.cursor()
+	except Exception, e:
+		traceback.print_exc(e)
+		build_connection()
+
+build_connection()
+
+def excute_update(sql):
+	global database_client_connection, database_client_cursor
+	try:
+		database_client_cursor.execute(sql)
+		database_client_connection.commit()
+	except Exception, e:
+		traceback.print_exc(e)
+		build_connection()
+		execute_update(sql)
+
+def execute_query(sql):
+	global database_client_connection, database_client_cursor
+	try:
+		database_client_cursor.execute(sql)
+		return database_client_cursor.fetchall()
+	except Exception, e:
+		traceback.print_exc(e)
+		build_connection()
+		return execute_query(sql)
+
+if __name__ == '__main__':
+	sql = "insert into SpotCheck values('%s','%s','%s','%s',now())" % ('文昌展宏水产专业合作社', '海南省', '', '469005NA000091X')
+	print sql
+	excute_update(sql)
